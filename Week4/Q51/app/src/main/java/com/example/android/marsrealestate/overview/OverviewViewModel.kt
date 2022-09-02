ae6feae36a0e49e375b928280aa773b13646e220
Tsai Cheng Han <tsaichenghan999@gmail.com>
2022-09-02 19:33:19 +0800

    add transformation

--- Week4/Q51/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ Week4/Q51/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -100,5 +100,22 @@ class OverviewViewModel() : ViewModel() {
         getMarsRealEstateProperties(filter)
     }
 
-    
+    val displayPropertyPrice = Transformations.map(selectedProperty) {
+        app.applicationContext.getString(
+            when (it.isRental) {
+                true -> R.string.display_price_monthly_rental
+                false -> R.string.display_price
+            }, it.price)
+    }
+    val displayPropertyType = Transformations.map(selectedProperty) {
+        app.applicationContext.getString(R.string.display_type,
+            app.applicationContext.getString(
+                when (it.isRental) {
+                    true -> R.string.type_rent
+                    false -> R.string.type_sale
+                }
+            )
+
+        )
+    }
 }
